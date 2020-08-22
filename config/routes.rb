89e732ea11236c391995009f28583bcea3c800a9
ahtapaehide <hidemarusu00biz@gmail.com>
2020-08-22 16:47:44 +0900

    indexをログイン画面に変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
+  get 'comments/index'
+  # get '/' => 'home#index'
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root 'comments#index' # ここを追記します
+  get 'comments/index' # 自動で設定されたルーティング
 end
