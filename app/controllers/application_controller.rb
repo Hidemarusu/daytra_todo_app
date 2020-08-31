f7166f1bb522faa1635bfd34574f7212c3800114
ahtapaehide <hidemarusu00biz@gmail.com>
2020-08-31 13:08:55 +0900

    ユーザーと記事の紐付け完了 表示しないようには出来ていないので、要修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user
       flash[:notice] = "ログインに成功しました" 
-      tasks_url  #　指定したいパスに変更
+      tasks_index_path  #　指定したいパスに変更
     else
       flash[:notice] = "新規登録完了しました。次に名前を入力してください" 
       new_profile_path  #　指定したいパスに変更
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   validates :title, length: {maximum: 20}, presence: true, uniqueness: true
+
+  belongs_to :user
 end
